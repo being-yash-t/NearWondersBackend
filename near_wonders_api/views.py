7af269aa7ee9064f37075792b7c3479fa5411ba7
Yash Thakur <yasht3210@gmail.com>
2024-06-14 00:50:39 +0530

    Added hello API

---
+++ near_wonders_api/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+class HelloWorld(APIView):
+    def get(self, request):
+        return Response({"message": "Hello, world!"}, status=status.HTTP_200_OK)
+
